a8d14f0d189043eb8e0a97cc1b936c70a87cc52d
khalidfasely <faselykhalid01@gmail.com>
2022-02-08 17:27:06 +0100

    Add allowed host

--- project3/settings.py
+++ project3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY_MAIL')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lek-mail.herokuapp.com']
 
 
 # Application definition
